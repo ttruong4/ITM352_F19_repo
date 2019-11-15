871e7daaa3b170463fd862392b8f20c2c4b4cb01
Terrance Truong <ttruong4@hawaii.edu>
2019-11-14 21:46:39 -1000

    commit

--- Truong_Terrance_Assignment1/server.js
+++ Truong_Terrance_Assignment1/server.js
@@ -52,8 +52,8 @@ app.listen(8080, () => console.log(`listening on port 8080`));
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume no errors at first
     if(q==""){q=0;}
-    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(q) != q) errors.push(' Not an integer!'); // Check that it is an integer
-    return returnErrors ? errors : (errors.length == 0);
+    if (Number(q) != q) errors.push('Not a number!'); // Checks if the user entered a number value
+    if (q < 0) errors.push('Negative value!'); // Checks if it is non-negative number 
+    if (parseInt(q) != q) errors.push(' Not an integer!'); // Check that it's an integer
+    return returnErrors ? errors : (errors.length == 0);//returns 0 if no amount was entered
 }
