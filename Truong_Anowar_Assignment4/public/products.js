8e9243c8490a33a5666c729bb18be2d0d4d87d4f
Terrance Truong <ttruong4@hawaii.edu>
2019-12-21 01:27:03 -1000

    final project

---
+++ Truong_Anowar_Assignment4/public/products.js
@@ -0,0 +1,37 @@
+products =
+[
+    {
+        "type": "Black Coffee",
+        "price": 2.00,
+        "image": "./images/coffee1.jpg"
+    },
+    {
+        "type": "Vanilla Latte",
+        "price": 5.00,
+        "image": "./images/coffee2.jpg"
+    },
+    {
+        "type": "Macchiato",
+        "price": 5.00,
+        "image": "./images/coffee3.jpg"
+    },
+    { 
+        "type": "Espresso",
+        "price": 4.00,
+        "image": "./images/coffee4.jpg"
+    },
+    {
+        "type": "Honey Coffee",
+        "price": 10.00,
+        "image": "./images/coffee5.jpg"
+    },
+    {
+        "type": "Pumpkin Spice Latte",
+        "price": 7.00,
+        "image": "./images/coffee6.jpg"
+    },
+];
+
+if(typeof module != 'undefined') {
+    module.exports = products;
+}
